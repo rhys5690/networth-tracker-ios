f8fe7bbf8e0b161b6988fdec1d7232a82f557ba9
rhys5690 <rhys5690@gmail.com>
2018-05-27 10:49:57 +1000

    Initial Commit

---
+++ networth-tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  networth-tracker
+//
+//  Created by Rhys Diab on 27/5/18.
+//  Copyright © 2018 Rhys Diab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
